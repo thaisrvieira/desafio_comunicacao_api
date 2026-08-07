a25895ad00ee7295df3a4d0d9baef3bd5b13abd9
Thais Vieira <thaisrvieira19@gmail.com>
2026-08-06 21:13:49 -0300

    Substitui ComunicacaoConverter por ComunicacaoMapper usando MapStruct

--- desafio_comunicacao_api/src/main/java/com/luizalebs/comunicacao_api/business/service/ComunicacaoService.java
+++ desafio_comunicacao_api/src/main/java/com/luizalebs/comunicacao_api/business/service/ComunicacaoService.java
@@ -3,7 +3,7 @@ package com.luizalebs.comunicacao_api.business.service;
 import com.luizalebs.comunicacao_api.api.dto.ComunicacaoInDTO;
 import com.luizalebs.comunicacao_api.api.dto.ComunicacaoOutDTO;
 import com.luizalebs.comunicacao_api.business.client.EmailNotificacaoClient;
-import com.luizalebs.comunicacao_api.business.converter.ComunicacaoConverter;
+import com.luizalebs.comunicacao_api.business.mapper.ComunicacaoMapper;
 import com.luizalebs.comunicacao_api.infraestructure.entities.ComunicacaoEntity;
 import com.luizalebs.comunicacao_api.infraestructure.enums.ModoEnvioEnum;
 import com.luizalebs.comunicacao_api.infraestructure.enums.StatusEnvioEnum;
@@ -16,12 +16,12 @@ import java.util.Objects;
 public class ComunicacaoService {
 
     private final ComunicacaoRepository repository;
-    private final ComunicacaoConverter converter;
+    private final ComunicacaoMapper mapper;
     private final EmailNotificacaoClient emailClient;
 
-    public ComunicacaoService(ComunicacaoRepository repository, ComunicacaoConverter converter, EmailNotificacaoClient emailClient) {
+    public ComunicacaoService(ComunicacaoRepository repository, ComunicacaoMapper mapper, EmailNotificacaoClient emailClient) {
         this.repository = repository;
-        this.converter = converter;
+        this.mapper = mapper;
         this.emailClient = emailClient;
     }
 
@@ -30,7 +30,7 @@ public class ComunicacaoService {
             throw new RuntimeException();
         }
         dto.setStatusEnvio(StatusEnvioEnum.PENDENTE);
-        ComunicacaoEntity entity = converter.paraEntity(dto);
+        ComunicacaoEntity entity = mapper.paraEntity(dto);
         repository.save(entity);
 
         if (entity.getModoDeEnvio() == ModoEnvioEnum.EMAIL) {
@@ -39,7 +39,7 @@ public class ComunicacaoService {
             repository.save(entity);
         }
 
-        ComunicacaoOutDTO outDTO = converter.paraDTO(entity);
+        ComunicacaoOutDTO outDTO = mapper.paraDTO(entity);
         return outDTO;
     }
 
@@ -48,7 +48,8 @@ public class ComunicacaoService {
         if (Objects.isNull(entity)) {
             throw new RuntimeException();
         }
-        return converter.paraDTO(entity);
+        return mapper.paraDTO(entity);
+
     }
 
     public ComunicacaoOutDTO alterarStatusComunicacao(String emailDestinatario) {
@@ -58,7 +59,8 @@ public class ComunicacaoService {
         }
         entity.setStatusEnvio(StatusEnvioEnum.CANCELADO);
         repository.save(entity);
-        return (converter.paraDTO(entity));
+        return (mapper.paraDTO(entity));
+
     }
 
 }
